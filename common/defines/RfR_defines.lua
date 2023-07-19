369ae93c7c6c488c1559b5be4a41b679967f254b
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2023-07-19 16:00:39 -0500

    --updated the starting techs & tank designs

---
+++ common/defines/RfR_defines.lua
@@ -0,0 +1,14 @@
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 25
+NDefines.NCountry.FEMALE_UNIT_LEADER_BASE_CHANCE = {1.0,1.0,1.0,1.0,1.0,}
+
+NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 20
+
+NDefines.NGame.START_DATE = "3015.1.1.12"
+NDefines.NGame.END_DATE = "3030.1.1.1"
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 1.433				-- Based on approximate distance from Skagen, Denmark to Messina, Italy
+NDefines.NGame.SAVE_VERSION = 10						-- 1.11.0 (Barbarossa)
+NDefines.NGame.FUEL_RESOURCE = "food"
+
+NDefines.NMilitary.NEW_COMMANDER_RANDOM_PERSONALITY_TRAIT_CHANCES = { 1, 0.5 }
+
+NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 5
